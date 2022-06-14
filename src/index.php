2b3b0c58fcf518b29c68f9d77a630d652004e1a0
Ignacio Núñez <JefaturaEstudios@itecfp.com>
2022-06-14 12:05:28 +0200

    Ahora lista

--- src/index.php
+++ src/index.php
@@ -1,7 +1,27 @@
-<?php
-    use ITEC\PRESENCIAL\DAW\BDLIST\bd;
-    include_once "../vendor/autoload.php";
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listas Bases de datos</title>
+</head>
+<body>
+    <h1>Listado de bases de datos disponibles</h1>
+    <?php
+        use ITEC\PRESENCIAL\DAW\BDLIST\bd;
+        include_once "../vendor/autoload.php";
 
-    $bd = new bd ();
-    $resultado = $bd->Select("SHOW DATABASES;");
-    print_r($resultado);
+        $bd = new bd ();
+        $resultado = $bd->Select("SHOW DATABASES;");
+        for($i=0;$i<count($resultado);$i++){
+            echo '<div><a href=database.php?db='.$resultado[$i]["Database"].'>'.$resultado[$i]["Database"].'</a></div>';
+        }
+    ?>
+
+
+</body>
+</html>
+
+
+
